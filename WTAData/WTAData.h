26ca843e389b5b5b904330a1891f2b08505a5e34
eriklamanna <erik.lamanna@willowtreeapps.com>
2014-12-10 11:15:01 -0500

    Add updated copyrights and documentation to interfaces.

--- WTAData/WTAData.h
+++ WTAData/WTAData.h
@@ -2,12 +2,38 @@
 //  WTAData.h
 //  WTAData
 //
-//  Copyright (c) 2014 WillowTreeApps. All rights reserved.
+//  Copyright (c) 2014 WillowTree, Inc.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
 //
 
 #import <CoreData/CoreData.h>
 #import <Foundation/Foundation.h>
 
+/**
+ WTAData provides a simplified interface to setting up an asynchronous CoreData stack.  WTAData 
+ utilizes two NSManagedObjectContexts: a main context generally used by UI binding and a background
+ context that is used to import data from an API for example.  The main context listens for
+ notifications of changes on the background context and will update when the background context
+ saves.  In most cases, saves will generally be done on the background context and the main context 
+ is used for reading the data and responding to changes.
+ */
 @interface WTAData : NSObject
 
 /// The current managed object model
@@ -29,9 +55,9 @@
  Initialize a data stack using the specified model. The name of the sqlite database will follow
  the name of the managed object.
  
- :param: modelName name of the managed object model to load
+ @param modelName name of the managed object model to load
  
- :returns: instance of the WTAData class
+ @return instance of the WTAData class
  */
 - (instancetype)initWithModelNamed:(NSString*)modelName;
 
@@ -39,10 +65,10 @@
  Initialize a data stack using the specified model. The name of the sqlite database will follow
  the name of the managed object. This function will also delete the core data store
  
- :param: modelName name of the managed object model to load
- :param: deleteOnModelMismatch delete the data store if the core data models do not match.
+ @param modelName name of the managed object model to load
+ @param deleteOnModelMismatch delete the data store if the core data models do not match.
  
- :returns: instance of the WTAData class
+ @return instance of the WTAData class
  */
 - (instancetype)initWithModelNamed:(NSString *)modelName
              deleteOnModelMismatch:(BOOL)deleteOnModelMismatch;
@@ -52,12 +78,12 @@
  the name of the managed object. This function will also delete the core data store if there is a 
  model mismatch, and will delete the store if it fails integrity checks
  
- :param: modelName name of the managed object model to load
- :param: deleteOnModelMismatch delete the data store if the core data models do not match.
- :param: verifyStoreIntegrity set to YES to perform a store integrity check and delete the data
+ @param modelName name of the managed object model to load
+ @param deleteOnModelMismatch delete the data store if the core data models do not match.
+ @param verifyStoreIntegrity set to YES to perform a store integrity check and delete the data
                               store if the check does not match.
  
- :returns: instance of the WTAData class
+ @return instance of the WTAData class
  */
 
 - (instancetype)initWithModelNamed:(NSString *)modelName
@@ -67,9 +93,9 @@
 /**
  Initialize an in-memory data stack using the specified model.
  
- :param: modelName name of the managed object model to load
+ @param modelName name of the managed object model to load
  
- :returns: instance of the WTAData class
+ @return instance of the WTAData class
  */
 - (instancetype)initInMemoryStackWithModelNamed:(NSString*)modelName;
 
@@ -82,10 +108,10 @@
 /**
  Perform save in the specified context.
  
- :param: context the context to save on
- :param: wait YES if the operation is blocking, NO for non-blocking
- :param: work the block that operates on the managed objects to save
- :param: completion block called when the save is complete
+ @param context the context to save on
+ @param wait YES if the operation is blocking, NO for non-blocking
+ @param work the block that operates on the managed objects to save
+ @param completion block called when the save is complete
  */
 - (void)saveInContext:(NSManagedObjectContext *)context
                  wait:(BOOL)wait
@@ -96,8 +122,8 @@
  Perform save in the background using the backgroundContext. Changes are then propagated to 
  the main context.
  
- :param: work the block that operates on the managed objects to save
- :param: completion block called when the save is complete
+ @param work the block that operates on the managed objects to save
+ @param completion block called when the save is complete
  */
 -(void)saveInBackground:(void (^)(NSManagedObjectContext *context))work
              completion:(void (^)(BOOL savedChanges, NSError *error))completion;
@@ -106,17 +132,17 @@
  Perform save in the background using the backgroundContext and wait for completion. Changes are 
  then propagated to the main context.
  
- :param: work the block that operates on the managed objects to save
+ @param work the block that operates on the managed objects to save
  */
 -(void)saveInBackgroundAndWait:(void (^)(NSManagedObjectContext *context))work;
 
 /**
  Perform save in the specified context.
  
- :param: context the context to save on
- :param: wait YES if the operation is blocking, NO for non-blocking
- :param: work the block that operates on the managed objects to save
- :param: completion block called when the save is complete
+ @param context the context to save on
+ @param wait YES if the operation is blocking, NO for non-blocking
+ @param work the block that operates on the managed objects to save
+ @param completion block called when the save is complete
  */
 - (void)deleteAllDataWithCompletion:(void (^)(NSError*))completion;
 
@@ -125,23 +151,23 @@
 /**
  Deletes all of the files associated with a given model store.
  
- :param: modelName name of the managed object model to remove stores for
+ @param modelName name of the managed object model to remove stores for
  */
 + (void)deleteStoreFilesForModelNamed:(NSString*)modelName;
 
 /**
  Returns the URL for the database created with the specified store name.
  
- :param: storeName the name of the persistent store that matches this database
+ @param storeName the name of the persistent store that matches this database
  
- :returns: the url for the database
+ @return the url for the database
  */
 + (NSURL*)databaseURLForStoreName:(NSString*)storeName;
 
 /** 
  Returns the name of the default store pulling from the bundle name.
  
- :returns: the default bundle store
+ @return the default bundle store
  */
 + (NSString*)defaultStoreName;
 
